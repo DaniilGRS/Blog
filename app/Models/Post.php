0696ef133dfdf0f5c4bff5ba5b781cf251dc575d
Веб стдудент дэмо 10 <w10@tk.ru>
2025-05-22 10:26:39 +0500

    13-18 практика

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -54,5 +55,10 @@ class Post extends Model
             return asset('images/default.jpg');
         }
         return asset("uploads/{$this->thumbnail}");
+    }
+
+    public function getPostDate()
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $this->created_at)->format('d F,Y');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\MainController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\TagsController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserPostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -37,3 +39,11 @@ Route::group(['middleware'=>'guest'], function (){
 Route::get('/login', [UserController::class, 'loginForm'])->name('login.create');
 Route::post('/login', [UserController::class, 'login'])->name('login');
 Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+
+Route::get('/',[UserPostController::class,'index'])->name('home');
+Route::get('/article/{slug}',[UserPostController::class,'show'])->name('posts.single');
+Route::get('/category/{slug}',[CategoryController::class,'show'])->name('categories.single');
+Route::get('/tag/{slug}',[TagsController::class,'show'])->name('tags.single');
+
+Route::get('/search', [SearchController::class, 'index'])->name('search');
+
